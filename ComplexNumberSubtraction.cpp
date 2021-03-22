db5e623a6e1be45918b170cd19e3c42f33f9893b
IgorQuint <igorquint@gmail.com>
2021-03-22 11:25:21 +0100

    Update ComplexNumberSubtraction.cpp

--- ComplexNumberSubtraction.cpp
+++ ComplexNumberSubtraction.cpp
@@ -26,7 +26,30 @@ class Complex
            return temp;
        }
 	   
-	   
-	   
 
+       void output()
+       {
+           if(imag < 0)
+               cout << "Output Complex number: "<< real << imag << "i";
+           else
+               cout << "Output Complex number: " << real << "+" << imag << "i";
+       }
+};
+
+int main()
+{
+    Complex c1, c2, result;
+
+    cout<<"Enter first complex number:\n";
+    c1.input();
+
+    cout<<"Enter second complex number:\n";
+    c2.input();
+
+    // In case of operator overloading of binary operators in C++ programming, 
+    // the object on right hand side of operator is always assumed as argument by compiler.
+    result = c1 - c2;
+    result.output();
+
+    return 0;
 }

13a1c12c228dc39fe260a12121617660e368459b
IgorQuint <igorquint@gmail.com>
2021-03-29 09:12:25 +0200

    Update Concat.cpp

--- Concat.cpp
+++ Concat.cpp
@@ -2,3 +2,17 @@
 #include <cstring>
 using namespace std;
 
+int main()
+{
+    char s1[50], s2[50];
+
+    cout << "Enter string 1: ";
+    cin.getline(s1, 50);
+
+    cout << "Enter string 2: ";
+    cin.getline(s2, 50);
+
+
+
+    return 0;
+}

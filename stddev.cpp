8ecf7fab01d55cd1f596f7e3b3d86ab08b20e204
IgorQuint <igorquint@gmail.com>
2021-04-02 07:40:31 +0200

    Update stddev.cpp

--- stddev.cpp
+++ stddev.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float calculateSD(float data[]);
+
+int main()
+{
+    int i;
+    float data[100];
+
+    cout << "Enter 100 values: ";
+    for(i = 0; i < 10; ++i)
+        cin >> data[i];
+
+    cout << endl << "Standard Deviation = " << calculateSD(data);
+
+    return 0;
+}

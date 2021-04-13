e06dc6537b4db76ada4ca80c78a2542d18855412
IgorQuint <igorquint@gmail.com>
2021-04-13 14:35:38 +0200

    Add MatrixMultiply cpp

--- stddev.cpp
+++ stddev.cpp
@@ -17,3 +17,21 @@ int main()
 
     return 0;
 }
+float calculateSD(float data[])
+{
+    float sum = 0.0, mean, standardDeviation = 0.0;
+
+    int i;
+
+    for(i = 0; i < 10; ++i)
+    {
+        sum += data[i];
+    }
+
+    mean = sum/10;
+
+    for(i = 0; i < 10; ++i)
+        standardDeviation += pow(data[i] - mean, 2);
+
+    return sqrt(standardDeviation / 10);
+}
